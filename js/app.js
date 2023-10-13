73befa9aa6298fe11c1b7b563294ccf6dffb1a61
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-13 14:14:48 +0200

    Add - functions to create and empty grid

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const gridDOMElement = document.querySelector('.row')
+
+const playDOMElement = document.querySelector('.play-btn')
+
+createGrid(100)
+
+function emptyGrid(){
+    gridDOMElement.innerHTML = ''
+}
+
+function createGrid(num){
+    for(let i = 0; i < num; i++){
+    const cellElement = `<div class="cell cell-10">${i+1}</div>`
+    console.log(cellElement)
+    gridDOMElement.innerHTML += cellElement
+    }
+}
