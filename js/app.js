80f9c553d89b93908c6a59073582e69d6f49341f
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-13 14:32:43 +0200

    Add - function and event listener for clicking cells

--- js/app.js
+++ js/app.js
@@ -5,7 +5,18 @@ const playDOMElement = document.querySelector('.play-btn')
 playDOMElement.addEventListener('click',function(){
     emptyGrid()
     createGrid(100)
+    const cellsArray = document.querySelectorAll('.cell')
+    for(let i = 0; i < cellsArray.length; i++){
+        const currentCell = cellsArray[i]
+        currentCell.addEventListener('click', clickCell)
+    }
 })
+
+function clickCell(){
+    const currentCellNumber = this.innerHTML
+    console.log('You clicked the cell number: ' + currentCellNumber)
+    this.classList.add('cell-clicked')
+}
 
 function emptyGrid(){
     gridDOMElement.innerHTML = ''
@@ -13,8 +24,8 @@ function emptyGrid(){
 
 function createGrid(num){
     for(let i = 0; i < num; i++){
-    const cellElement = `<div class="cell cell-10">${i+1}</div>`
-    console.log(cellElement)
-    gridDOMElement.innerHTML += cellElement
+        const cellElement = `<div class="cell cell-10">${i+1}</div>`
+        gridDOMElement.innerHTML += cellElement
     }
 }
+
